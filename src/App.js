8a112447a445e9c286112428f2433c77778d9461
yvonnesimonewatts <yvonnewatts@mail.com>
2021-02-18 16:18:12 +0000

    install Bootstrap v5.0.0 and react-bootstrap-icons
    fix: clean up react folders
    add: footer with open-source link to GitHub

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import * as Icon from 'react-bootstrap-icons';
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+      <header className="App-header">
+        <h1>React Weather App</h1>
+      </header>
+  
+        <footer>
+          <Icon.Github className="Github-icon"/>{" "}
+        <strong>
+          <a
+            className="Footer-link"
+            href="https://github.com/yvonnesimonewatts/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+        </strong>
+        by Yvonne Simone
+      </footer>
+      </div>
+
     </div>
   );
 }
